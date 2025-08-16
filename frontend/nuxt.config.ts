52281962ed341e81c587ddbf8aa4a6381fa417a7
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-08-16 01:10:06 +0000

    fix: update project name in bun.lock and clean up nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
+  modules: ["@nuxt/ui"],
   css: ["~/assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
   },
-  modules: ["@nuxt/ui"],
 });
