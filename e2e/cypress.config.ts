c16828b7c182af7c8e8b270c0d76598dac6ce1e7
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-08-17 02:41:13 +0000

    fix: update .gitignore and cypress.config for environment consistency; add service aliases in YAML

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://infokes.docker.localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.ts",
     viewportWidth: 1280,
     viewportHeight: 800,
-    video: false,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       return config;
